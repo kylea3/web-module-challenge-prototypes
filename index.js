817817abdf90883862829d7b7245fa5b9cc0f263
Kyle Andrus <kyleandrus3@gmail.com>
2022-11-15 17:05:06 -0700

    completed task 3

--- index.js
+++ index.js
@@ -75,10 +75,16 @@ Car.prototype.fill = function(gallons) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Person.prototype;
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
